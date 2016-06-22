2585cca7266b9fec2039e83ff08827e4abbe6965
U-WEB\rpeinado <rpeinado@BALT9-DEV-1087.web.web.com>
2016-06-22 15:42:31 -0300

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# lnframe
+
+## What it does
+
+This extension removes the LN Login modal frame
---
+++ lnframemain.js
@@ -0,0 +1,2 @@
+document.body.style.border = "5px solid blue";
+document.getElementsByClassName("lnmodal")[0].style.display = "none";
